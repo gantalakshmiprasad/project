9f5e45aeee59ac21288ca06d7ec89bfbd9b40ce5
master <gantalakshmiprasad@gmail.com>
2026-03-18 13:25:25 +0530

    fdfdf

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,10 @@ import 'package:firstproject/view/Authentication/signup.dart';
 import 'package:firstproject/view/Authentication/verificationpage.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get_storage/get_storage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await GetStorage.init();
+
   try {
     await Get.putAsync<AuthServices>(
       () async => AuthServices(),
